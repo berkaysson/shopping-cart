3b8b540089317f5358235ebf23dc67e59861473c
berkaysson <berkaysonel85@gmail.com>
2023-04-14 18:44:34 +0300

    Create Home react component

---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+const Home = () => {
+  return (
+    <div>
+      <div>
+        Welcome to Biome - your one-stop-shop for sustainable and eco-friendly
+        living.
+      </div>
+      <div>
+        Shop sustainably and make a difference with Biome - your destination for
+        eco-friendly living.
+      </div>
+      <button>Shop Now</button>
+    </div>
+  );
+};
+
+export default Home;
