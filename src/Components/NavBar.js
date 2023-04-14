6b8aaa65cf14a2d73f589032ae82a4f1b68f87e9
berkaysson <berkaysonel85@gmail.com>
2023-04-14 18:44:43 +0300

    Create NavBar react component

---
+++ src/Components/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <div>
+      <a>Logo-Biome</a>
+      <a>Home</a>
+      <a>Products</a>
+      <a>Contact</a>
+      <button>Cart</button>
+    </div>
+  )
+}
+
+export default NavBar;
