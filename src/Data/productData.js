da05773bdb84e95e507f1cf01d04eb466d82a885
berkaysson <berkaysonel85@gmail.com>
2023-04-14 18:45:25 +0300

    Create producData objects for each products

---
+++ src/Data/productData.js
@@ -0,0 +1,53 @@
+const productData = {
+  waterBottle: {
+    id: 1,
+    price: 29.95,
+    imgPath: "/images/water-bottle.jpg",
+    name: "Stainless Steel Water Bottle",
+    description: "Durable and reusable.",
+  },
+  produceBags: {
+    id: 2,
+    price: 24.95,
+    imgPath: "/images/produce-bags.jpg",
+    name: "Reusable Produce Bags",
+    description: "Eco-friendly and sustainable.",
+  },
+  cutlerySet: {
+    id: 3,
+    price: 14.95,
+    imgPath: "/images/cutlery-set.jpg",
+    name: "Bamboo Cutlery Set",
+    description: "Portable and stylish.",
+  },
+  beeswaxWraps: {
+    id: 4,
+    price: 19.95,
+    imgPath: "/images/beeswax-wraps.jpg",
+    name: "Beeswax Wraps",
+    description: "Sustainable alternative to plastic wrap.",
+  },
+  shampooBar: {
+    id: 5,
+    price: 12.95,
+    imgPath: "/images/shampoo-bar.jpg",
+    name: "Solid Shampoo Bar",
+    description: "Natural and gentle on hair.",
+  },
+  coffeeCup: {
+    id: 6,
+    price: 18.95,
+    imgPath: "/images/coffee-cup.jpg",
+    name: "Reusable Coffee Cup",
+    description: "Stylish and eco-friendly.",
+  },
+  dogWasteBags: {
+    id: 7,
+    price: 9.95,
+    imgPath: "/images/dog-waste-bags.jpg",
+    name: "Compostable Dog Waste Bags",
+    description: "Eco-friendly and biodegradable.",
+  },
+};
+
+export default productData;
