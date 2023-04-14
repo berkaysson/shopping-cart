ada66ce43338d3630d1774cec876340c8aafa8f2
berkaysson <berkaysonel85@gmail.com>
2023-04-14 18:44:26 +0300

    Create Contact react component

---
+++ src/Components/Contact.js
@@ -0,0 +1,43 @@
+export const Contact = () => {
+  return (
+    <div>
+      <p>
+        If you have any questions or comments, please feel free to get in touch
+        with us. You can contact us through the form below or email us directly
+        at support@ecofriendlyshoppingco.com. Our customer service team will
+        respond to your inquiry within 24-48 hours.
+      </p>
+      <div>
+        <h3>Mailing Address:</h3>
+        <p>
+          Eco-Friendly Shopping Co. <br />
+          1234 Green Street <br />
+          New York, NY 10001 <br />
+          USA
+        </p>
+      </div>
+      <div>
+        <h3>Phone:</h3>
+        <p>1-800-ECO-1234</p>
+      </div>
+      <div>
+        <h3>Email:</h3>
+        <p>support@ecofriendlyshoppingco.com</p>
+      </div>
+      <div>
+        <h3>Follow Us:</h3>
+        <ul>
+          <li>
+            <a href="https://www.facebook.com/ecofriendlyshoppingco">Facebook</a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/ecofriendlyshoppingco">Instagram</a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com/ecofriendlyshop">Twitter</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
