08c0fe6218e1b9ddbde1ce9135d551cf006e5552
berkaysson <berkaysonel85@gmail.com>
2023-04-14 23:40:42 +0300

    Add functional, stylish NavBar by using styled-components and react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import { Products } from "./Components/Product/Products";
+import NavBar from "./Components/NavBar";
+import { Cart } from "./Components/Cart";
+import { Contact } from "./Components/Contact";
+
+import styled from "styled-components";
+
+const ContentWrapper = styled.div`
+  padding: var(--padding);
+`;
+
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <ContentWrapper>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </ContentWrapper>
+      </div>
+    </Router>
   );
 }
 
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,13 +1,71 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+const NavBarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: var(--padding);
+  background-color: var(--dark-color);
+  color: var(--light-color);
+`;
+
+const Logo = styled(NavLink)`
+  font-size: var(--font-xlg);
+  font-weight: bold;
+  color: var(--light-color);
+  text-decoration: none;
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const NavLinkItem = styled(NavLink)`
+  margin-left: var(--margin);
+  font-size: var(--font-lg);
+  color: var(--light-color);
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  &.active{
+    color: var(--light-alt-color);
+    text-decoration: underline;
+  }
+`;
+
+const CartButton = styled.button`
+  font-size: var(--font-lg);
+  padding: var(--btn-padding);
+  margin-left: var(--margin);
+  cursor: pointer;
+  background-color: var(--theme-color);
+  color: var(--light-color);
+  border: none;
+  border-radius: var(--border-radius);
+  text-decoration: none;
+
+  &:hover {
+    background-color: var(--dark-alt-color);
+  }
+`;
+
 const NavBar = () => {
   return (
-    <div>
-      <a>Logo-Biome</a>
-      <a>Home</a>
-      <a>Products</a>
-      <a>Contact</a>
-      <button>Cart</button>
-    </div>
-  )
-}
+    <NavBarContainer>
+      <Logo to="/">Logo-Biome</Logo>
+      <NavLinks>
+        <NavLinkItem to="/" activeClassName="active" >Home</NavLinkItem>
+        <NavLinkItem to="/products" activeClassName="active">Products</NavLinkItem>
+        <NavLinkItem to="/contact" activeClassName="active">Contact</NavLinkItem>
+        <CartButton>Cart</CartButton>
+      </NavLinks>
+    </NavBarContainer>
+  );
+};
 
 export default NavBar;
