2fe9fb346a9d67b5496ab7d91f180e611bd562ad
berkaysson <berkaysonel85@gmail.com>
2023-04-15 19:18:32 +0300

    Style Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,71 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  color: var(--dark-color);
+  padding: var(--padding-lg);
+`;
+
+const HomeTitle = styled.div`
+  font-size: var(--font-xlg);
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: var(--gap);
+`;
+
+const HomeSubtitle = styled.div`
+  font-size: var(--font-lg);
+  text-align: center;
+  margin-bottom: var(--padding);
+`;
+
+const ShopButton = styled(Link)`
+  font-size: var(--font-lg);
+  padding: var(--btn-padding);
+  margin-top: var(--gap);
+  cursor: pointer;
+  background-color: var(--theme-color);
+  color: var(--light-color);
+  border: none;
+  border-radius: var(--border-radius);
+  text-decoration: none;
+
+  &:hover {
+    background-color: var(--dark-alt-color);
+  }
+`;
+
+const HomeContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: var(--padding);
+  border-radius: var(--border-radius);
+  backdrop-filter: blur(5px);
+  box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
+`
+
 const Home = () => {
   return (
-    <div>
-      <div>
+    <HomeContainer>
+      <HomeContentWrapper>
+      <HomeTitle>
         Welcome to Biome - your one-stop-shop for sustainable and eco-friendly
         living.
-      </div>
-      <div>
+      </HomeTitle>
+      <HomeSubtitle>
         Shop sustainably and make a difference with Biome - your destination for
         eco-friendly living.
-      </div>
-      <button>Shop Now</button>
-    </div>
+      </HomeSubtitle>
+      <ShopButton to="/products">Shop Now</ShopButton>
+      </HomeContentWrapper>
+    </HomeContainer>
   );
 };
 
