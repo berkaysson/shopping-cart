e17234089627d60054792c1ebb1a86605f412b9a
berkaysson <berkaysonel85@gmail.com>
2023-04-15 19:18:23 +0300

    Style Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,65 @@
+import styled from "styled-components";
+
+const ContactContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: var(--padding-lg);
+  background-color: transparent;
+  border-radius: var(--border-radius);
+  backdrop-filter: blur(5px);
+  box-shadow: rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;
+
+  h2 {
+    font-size: var(--font-xlg);
+    margin-bottom: 2rem;
+    text-align: center;
+  }
+
+  p {
+    font-size: var(--font-md);
+    margin-bottom: 1.5rem;
+    text-align: center;
+  }
+
+  div {
+    margin-bottom: 1.5rem;
+
+    h3 {
+      font-size: var(--font-lg);
+      margin-bottom: 1rem;
+    }
+
+    p {
+      font-size: var(--font-md);
+      line-height: 1.5;
+      margin-bottom: 0.5rem;
+    }
+
+    ul {
+      display: flex;
+      list-style: none;
+      padding: 0;
+
+      li {
+        margin-right: 1rem;
+
+        a {
+          color: var(--dark-color);
+
+          &:hover {
+            text-decoration: underline;
+          }
+        }
+      }
+    }
+  }
+`;
+
+
 export const Contact = () => {
   return (
-    <div>
+    <ContactContainer>
       <p>
         If you have any questions or comments, please feel free to get in touch
         with us. You can contact us through the form below or email us directly
@@ -38,6 +97,7 @@ export const Contact = () => {
           </li>
         </ul>
       </div>
-    </div>
+    </ContactContainer>
   );
 };
+
