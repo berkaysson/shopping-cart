8a0d7e2e5d4aacc597789cd407b6e810fb371a44
berkaysson <berkaysonel85@gmail.com>
2023-04-15 19:19:13 +0300

    Style Products

--- src/Components/Product/Products.js
+++ src/Components/Product/Products.js
@@ -1,19 +1,40 @@
+import styled from "styled-components";
 import productData from "../../Data/productData";
 import { ProductItem } from "./ProductItem";
 
-export const Products = () => {
+export const Products = ({onAddCartItem}) => {
   return (
-    <div>
+    <ProductsWrapper>
       {Object.values(productData).map((product) => (
         <ProductItem
           key={product.id}
+          product={product}
           id={product.id}
           price={product.price}
           imgPath={product.imgPath}
           name={product.name}
           description={product.description}
+          onAddCartItem={onAddCartItem}
         />
       ))}
-    </div>
+    </ProductsWrapper>
   );
 };
+
+const ProductsWrapper = styled.div`
+  display: grid;
+  align-items: flex-start;
+  justify-content: center;
+  justify-items: center;
+  min-width: 100%;
+  height: 100%;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-gap: var(--gap-lg);
+  padding: var(--padding);
+  margin-top: 5rem;
+
+  @media (max-width: 900px) {
+    grid-template-columns: repeat(1, minmax(200px, 1fr));
+    margin-top: 2rem;
+  }
+`
