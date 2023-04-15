f56330b3fbf1cb7ef200d844e73c9ccceb856829
berkaysson <berkaysonel85@gmail.com>
2023-04-15 19:17:39 +0300

    Add Cart and CartItem

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,3 +1,36 @@
-export const Cart = () => {
-  return
-}
+import styled from "styled-components";
+
+import CartItem from "./CartItem";
+
+const CartContainer = styled.div`
+  padding: var(--padding);
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: 1rem;
+  background-color: var(--primary-color);
+  color: var(--light-color);
+  border: none;
+  cursor: pointer;
+  font-weight: bold;
+`;
+
+const CartContent = styled.div`
+  padding: 1rem;
+`;
+
+export const Cart = ({ showCart, cartData, removeItem }) => {
+  return (
+    <CartContainer>
+      <CloseButton onClick={showCart}>Close</CloseButton>
+      <CartContent>
+        {cartData.map((item) => (
+          <CartItem key={item.id} item={item} removeItem={removeItem} />
+        ))}
+      </CartContent>
+    </CartContainer>
+  );
+};
---
+++ src/Components/CartItem.js
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+
+const CartItemWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+`;
+
+const ItemInfoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ItemImage = styled.img`
+  width: 50px;
+  height: 50px;
+  margin-right: 1rem;
+`;
+
+const ItemTitle = styled.p`
+  font-size: 1rem;
+  font-weight: bold;
+  margin-bottom: 0.25rem;
+`;
+
+const ItemPrice = styled.p`
+  font-size: 0.8rem;
+  color: var(--light-color);
+`;
+
+const RemoveButton = styled.button`
+  font-size: 0.8rem;
+  color: var(--primary-color);
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+const CartItem = ({ item, removeItem }) => {
+  return (
+    <CartItemWrapper>
+      <ItemInfoWrapper>
+        <ItemImage src={item.image} alt={item.title} />
+        <div>
+          <ItemTitle>{item.title}</ItemTitle>
+          <ItemPrice>${item.price}</ItemPrice>
+        </div>
+      </ItemInfoWrapper>
+      <RemoveButton onClick={() => removeItem(item.id)}>Remove</RemoveButton>
+    </CartItemWrapper>
+  );
+};
+
+export default CartItem;
