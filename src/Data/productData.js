2abd97fea5a4138d50c882c0cce0e8ebc851c8e6
berkaysson <berkaysonel85@gmail.com>
2023-04-16 15:56:58 +0300

    import images

--- src/Data/productData.js
+++ src/Data/productData.js
@@ -1,50 +1,58 @@
+import waterBottleImg from '../Data/images/waterbottle.jpg';
+import produceBagImg from '../Data/images/producebag.jpeg';
+import cutlerySetImg from '../Data/images/cutleryset3.jpg';
+import beeswaxWrapsImg from '../Data/images/wraps.jpeg';
+import shampooBarImg from '../Data/images/shampoobar.jpg';
+import coffeeCupImg from '../Data/images/coffeecup.jpg';
+import dogWasteBagsImg from '../Data/images/dogbag.jpg';
+
 const productData = {
   waterBottle: {
     id: 1,
     price: 29.95,
-    imgPath: "/images/water-bottle.jpg",
+    imgPath: waterBottleImg,
     name: "Stainless Steel Water Bottle",
     description: "Durable and reusable.",
   },
   produceBags: {
     id: 2,
     price: 24.95,
-    imgPath: "/images/produce-bags.jpg",
+    imgPath: produceBagImg,
     name: "Reusable Produce Bags",
     description: "Eco-friendly and sustainable.",
   },
   cutlerySet: {
     id: 3,
     price: 14.95,
-    imgPath: "/images/cutlery-set.jpg",
+    imgPath: cutlerySetImg,
     name: "Bamboo Cutlery Set",
     description: "Portable and stylish.",
   },
   beeswaxWraps: {
     id: 4,
     price: 19.95,
-    imgPath: "/images/beeswax-wraps.jpg",
+    imgPath: beeswaxWrapsImg,
     name: "Beeswax Wraps",
     description: "Sustainable alternative to plastic wrap.",
   },
   shampooBar: {
     id: 5,
     price: 12.95,
-    imgPath: "/images/shampoo-bar.jpg",
+    imgPath: shampooBarImg,
     name: "Solid Shampoo Bar",
     description: "Natural and gentle on hair.",
   },
   coffeeCup: {
     id: 6,
     price: 18.95,
-    imgPath: "/images/coffee-cup.jpg",
+    imgPath: coffeeCupImg,
     name: "Reusable Coffee Cup",
     description: "Stylish and eco-friendly.",
   },
   dogWasteBags: {
     id: 7,
     price: 9.95,
-    imgPath: "/images/dog-waste-bags.jpg",
+    imgPath: dogWasteBagsImg,
     name: "Compostable Dog Waste Bags",
     description: "Eco-friendly and biodegradable.",
   },
